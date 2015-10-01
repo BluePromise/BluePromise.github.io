29191c7466149d95b65ff47cac903cb3c8c4ea7a
Daniel Williams <Zazu1914@gmail.com>
2015-10-01 10:40:18 -0400

    Changed the javascript to work in reverse, should both hide text on Sigma Beta Club page as well as make it work on the first click now. small early update

--- js/sigma-beta-club.js
+++ js/sigma-beta-club.js
@@ -1,44 +1,44 @@
 function ShowEducation() {
-    if (document.getElementById("education-text").style.display === "none") {
-    	document.getElementById("education-text").style.display = "block"; 
+    if (document.getElementById("education-text").style.display === "block") {
+    	document.getElementById("education-text").style.display = "none"; 
     }
     else {
-    	document.getElementById("education-text").style.display = "none";
+    	document.getElementById("education-text").style.display = "block";
     }
 }
 
 function ShowSocialAction() {
-    if (document.getElementById("social-action-text").style.display === "none") {
-    	document.getElementById("social-action-text").style.display = "block"; 
+    if (document.getElementById("social-action-text").style.display === "block") {
+    	document.getElementById("social-action-text").style.display = "none"; 
     }
     else {
-    	document.getElementById("social-action-text").style.display = "none";
+    	document.getElementById("social-action-text").style.display = "block";
     }
 }		
 
 function ShowBiggerAndBetterBusiness() {
-    if (document.getElementById("bigger-and-better-business-text").style.display === "none") {
-    	document.getElementById("bigger-and-better-business-text").style.display = "block"; 
+    if (document.getElementById("bigger-and-better-business-text").style.display === "block") {
+    	document.getElementById("bigger-and-better-business-text").style.display = "none"; 
     }
     else {
-    	document.getElementById("bigger-and-better-business-text").style.display = "none";
+    	document.getElementById("bigger-and-better-business-text").style.display = "block";
     }
 }
 
 function ShowChildhoodObesity() {
-    if (document.getElementById("childhood-obesity-text").style.display === "none") {
-    	document.getElementById("childhood-obesity-text").style.display = "block"; 
+    if (document.getElementById("childhood-obesity-text").style.display === "block") {
+    	document.getElementById("childhood-obesity-text").style.display = "none"; 
     }
     else {
-    	document.getElementById("childhood-obesity-text").style.display = "none";
+    	document.getElementById("childhood-obesity-text").style.display = "block";
     }
 }	
 
 function ShowSpecialPrograms() {
-    if (document.getElementById("special-programs-text").style.display === "none") {
-    	document.getElementById("special-programs-text").style.display = "block"; 
+    if (document.getElementById("special-programs-text").style.display === "block") {
+    	document.getElementById("special-programs-text").style.display = "none"; 
     }
     else {
-    	document.getElementById("special-programs-text").style.display = "none";
+    	document.getElementById("special-programs-text").style.display = "block";
     }
 }	
