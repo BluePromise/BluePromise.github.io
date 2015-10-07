7a52036ffd5ea299874d7e44cb644d26d523ecbf
Daniel Williams <Zazu1914@gmail.com>
2015-10-07 15:19:36 -0400

    Removed contact form from page until I can better understand php servers

--- comment.php
+++ comment.php
@@ -19,3 +19,78 @@ if(isset($_POST['submit'])){
     // You cannot use header and echo together. It's one or the other.
     }
 ?>
+
+html code
+
+<form name="contactform" method="post" action="../contact.php">
+        <div id="submission" class="row">
+          
+          <div class="col-lg-1">
+          </div>
+          
+          <div class="col-lg-7">
+            
+            <div class="input-title row">
+              <h3>Full Name<span class="red">*</span></h3>
+            </div>          
+            <div id="firstname" class="row">
+              <div class="col-lg-6">
+                <input type="text" name="first_name" maxlength="50" size="30">
+                <p>First</p>
+              </div>
+              <div class="col-lg-6">
+                <input type="text" name="last_name" maxlength="50" size="30">
+                <p>Last</p>
+              </div>
+            </div>
+
+            <div class="input-title row">
+              <h3>Email<span class="red">*</span></h3>
+            </div>          
+            <div id="email" class="row">
+              <div class="col-lg-6">
+                <input type="text" name="email" maxlength="80" size="30">
+                <p>Enter Email</p>
+              </div>
+              <div class="col-lg-6">
+                <input type="text" name="email-confirm" maxlength="80" size="30">
+                <p>Confirm Email</p>
+              </div>
+            </div>
+
+            <div class="input-title row">
+              <h3>Phone Number</h3>
+            </div>
+            <div id="phonenumber" class="row">
+              <div class="col-lg-6">
+                <input type="text" name="telephone" maxlength="30" size="30">
+              </div>
+            </div>
+
+            <div class="input-title row">
+              <h3>Questions or Comments</h3>
+            </div>
+            <div id="questions" class="row">
+                <input type="text" maxlength="1000" cols="25" rows="6">
+            </div>
+            <div class="input-title row">
+              <input id="submit" type="submit" value="Submit">
+            </div>
+
+          </div>
+
+          <div class="col-lg-3">
+          </div>
+
+          <div class="col-lg-1">
+          </div>
+
+        </div><!--row-->
+
+      </form>
+        
+      <form name="contactform" method="post" action="test.php">
+        <div class="input-title row">
+          <input id="submit" type="submit" value="Test">
+        </div>
+      </form>
